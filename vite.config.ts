4eb8fc7a58f01b1b1d81785da17b4e703fb83e1a
Loïc Payol <contact@loicpayol.fr>
2024-10-21 21:18:29 +0200

    feat: add GlitchTip

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,16 @@
+import { sentrySvelteKit } from '@sentry/sveltekit';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit()]
+	plugins: [
+		sentrySvelteKit({
+			sourceMapsUploadOptions: {
+				org: 'loicpayolfr',
+				project: 'loicpayolfr',
+				url: 'https://10523fec1e7348388fdfc603695f2633@app.glitchtip.com/'
+			}
+		}),
+		sveltekit()
+	]
 });
